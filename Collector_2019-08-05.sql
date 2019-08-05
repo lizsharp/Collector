895fb8065983c22b85b82a340378449096e8fb59
Liz <ejgsharp@gmail.com>
2019-08-05 13:26:47 +0100

    Created db

---
+++ Collector_2019-08-05.sql
@@ -0,0 +1,57 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 192.168.20.20 (MySQL 5.6.44)
+# Database: Collector
+# Generation Time: 2019-08-05 12:12:03 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table garden plants
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `garden plants`;
+
+CREATE TABLE `garden plants` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `plant_name` varchar(255) NOT NULL DEFAULT '',
+  `plant_type` varchar(255) NOT NULL DEFAULT '',
+  `position` varchar(255) NOT NULL DEFAULT '',
+  `soil_type` varchar(255) NOT NULL DEFAULT '',
+  `colour` varchar(255) DEFAULT NULL,
+  `cost` float unsigned NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `garden plants` WRITE;
+/*!40000 ALTER TABLE `garden plants` DISABLE KEYS */;
+
+INSERT INTO `garden plants` (`id`, `plant_name`, `plant_type`, `position`, `soil_type`, `colour`, `cost`, `image`)
+VALUES
+	(1,'','','','',NULL,0,NULL);
+
+/*!40000 ALTER TABLE `garden plants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
