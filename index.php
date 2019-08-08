3b4d99db67769d1aed3f3543928ff426b62c813e
Liz <ejgsharp@gmail.com>
2019-08-08 10:38:15 +0100

    connected database to index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+$db = new PDO (
+    'mysql:host=192.168.20.20; dbname=Collector',
+    'root',
+    ''
+);
+
+$db->setAttribute(
+    PDO::ATTR_DEFAULT_FETCH_MODE,
+    PDO::FETCH_ASSOC
+);
+
+echo 'Hello';
