fa035e1846539a41a96649071a5a2f24e23309ef
Liz <ejgsharp@gmail.com>
2019-08-08 13:26:33 +0100

    Created normalize.css and style.css files. Added HTML boilerplate to index.php file. Added test content to check it worked.

--- index.php
+++ index.php
@@ -17,6 +17,25 @@ $sql-> execute();
 
 $results=$sql->fetchAll();
 
-var_dump ($results);
+//var_dump ($results);
 
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>The Gardeners Collection</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<p>test this works</p>
+<p> and that this works too</p>
+
+</body>
+
+</head>
+</html>
